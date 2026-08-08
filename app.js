bc5c0a6c2bba4910a238e92c370d5b346a30c53a
noorkhadija557 <noorkhadija557@gmail.com>
2026-08-08 22:39:00 +0500

    push

---
+++ app.js
@@ -0,0 +1,36 @@
+const supabaseUrl = "https://xmexfecjjalkhqtrlzzj.supabase.co"
+const supabaseKey = "sb_publishable_MscDQGxX8gej_btcdCaQjA_6qODt-W8"
+
+
+const { createClient } = supabase
+const client = createClient(supabaseUrl, supabaseKey)
+
+console.log(client);
+
+// collecting data
+
+let submit = document.querySelector("#submitBtn")
+
+let studentNames = document.querySelector("#stu")
+let course = document.querySelector("#cou")
+let email = document.querySelector("#email")
+
+
+submit.addEventListener("click", async (event) => {
+    try {
+        event.preventDefault()
+
+        const { error } = await client
+            .from(' ')
+            .insert([{
+                name: studentNames.value,
+                course: course.value,
+                email_address: email.value,
+
+            }])
+            console.log(error)
+    }
+    catch (error) {
+        // console.log(error)
+    }
+})
